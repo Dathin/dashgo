552539f385d4672b4e7afc274ca3bdab4cf437e7
Pedro Caires <pedro.caires@iteris.com.br>
2021-05-20 00:44:29 -0300

    feat: componentização de input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
+import {Input} from '../components/Form/Input';
+import { Button, Flex, FormLabel, Stack, FormControl } from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex as="form" w="100%" maxW={360} bg="gray.800" p={8} borderRadius={8} flexDir="column">
+        <Stack spacing="4">
+            <Input name="email" type="email" label="E-mail"></Input>
+            <Input name="password" type="password" label="Senha"></Input>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
