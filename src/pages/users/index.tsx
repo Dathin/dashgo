632669526c8b0b36e31d32edd052846d79a53a34
Pedro Caires <pedro.caires@iteris.com.br>
2021-06-22 23:52:17 -0300

    feat: lista de usuários

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,63 @@
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
+import React from "react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList(){
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <Sidebar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+
+                    
+                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon fontSize="20" as={RiAddLine}/>}>Criar novo</Button>
+                    </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>
+                                    Usuário
+                                </Th>
+                                <Th>
+                                    Data de cadastro
+                                </Th>
+                                <Th w="8">
+                                </Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />  
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Pedro Caires</Text>
+                                        <Text fontSize="sm" color="gray.300">pedromcaires99@gmail.com</Text>
+                                    </Box> 
+                                </Td>
+                                <Td>
+                                    27/07/2021
+                                </Td>
+                                <Td>
+                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon fontSize="16" as={RiPencilLine}/>}>Editar</Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
