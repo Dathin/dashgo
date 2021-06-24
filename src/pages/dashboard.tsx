097d859de3b70f021bba2b831e1b25f278f259ac
Pedro Caires <pedro.caires@iteris.com.br>
2021-06-23 23:36:42 -0300

    feat: dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -65,11 +65,11 @@ export default function Dashboard(){
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar></Sidebar>
                 <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-                    <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+                    <Box p={["3", "8"]} bg="gray.800" borderRadius={8} pb="4">
                         <Text fontSize="lg" mb="4" >Inscritos da semana</Text>
                         <Chart options={options} series={series} type="area" height={160}></Chart>
                     </Box>
-                    <Box p="8" bg="gray.800" borderRadius={8}>
+                    <Box p={["3", "8"]} bg="gray.800" borderRadius={8}>
                         <Text fontSize="lg" mb="4" >Taxa de abertura</Text>
                         <Chart options={options} series={series} type="area" height={160}></Chart>
                     </Box>
